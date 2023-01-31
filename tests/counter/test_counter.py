ce8b09faa5dd359eeae2e4bf4c65046a270a95c2
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-31 19:57:16 -0300

    feat: req10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,15 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+import pytest
 
 
 def test_counter():
-    pass
+    python_word_ocurrences = count_ocurrences("data/jobs.csv", "Python")
+    js_word_ocurrences = count_ocurrences("data/jobs.csv", "Javascript")
+    assert python_word_ocurrences == 1639
+    assert js_word_ocurrences == 122
+    with pytest.raises(TypeError):
+        count_ocurrences('data/jobs.csv')
+    with pytest.raises(TypeError):
+        count_ocurrences('Javascript')
+    with pytest.raises(TypeError):
+        count_ocurrences()
